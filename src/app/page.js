b2fa015bd882aab871b146b7e6db3f201fa0408a
quan2910 <ngtruongquan@gmail.com>
2024-08-21 14:18:42 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,30 @@
-'use client';
-import { useEffect } from 'react';
-import Lenis from 'lenis'
-import Intro from '@/components/Intro';
-import Description from '@/components/Description';
-import Section from '@/components/Section';
-export default function Home() {
+"use client";
 
-  useEffect( () => {
-    const lenis = new Lenis()
+import { useEffect } from "react";
+import Lenis from "lenis";
+import Intro from "@/components/Intro";
+import Description from "@/components/Description";
+import Section from "@/components/Section";
+const Home = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
 
     function raf(time) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
+      lenis.raf(time);
+      requestAnimationFrame(raf);
     }
 
-    requestAnimationFrame(raf)
-  }, [])
+    requestAnimationFrame(raf);
+  }, []);
 
   return (
     <main>
       <Intro />
       <Description />
       <Section />
-      <div className='h-screen'></div>
+      <div className="h-screen"></div>
     </main>
   );
-}
+};
+
+export default Home;
